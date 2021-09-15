b222c0fb65257a94d2cc98f765279dbd8def6fa9
jcrichard <jcharles.richard@gmail.com>
2021-09-15 23:03:17 +0100

    fix bound

--- pyrb/solvers.py
+++ pyrb/solvers.py
@@ -51,7 +51,7 @@ def _cycle(x, c, var, _varphi, sigma_x, Sx, budgets, pi, bounds, lambda_log, cov
         gamma_ = -lambda_log * budgets[i] * sigma_x
         x_tilde = (-beta + np.sqrt(beta ** 2 - 4 * alpha * gamma_)) / (2 * alpha)
 
-        x_tilde = np.maximum(np.minimum(x_tilde, bounds[i, 1.0]), bounds[i, 0.0])
+        x_tilde = np.maximum(np.minimum(x_tilde, bounds[i, 1]), bounds[i, 0])
 
         x[i] = x_tilde
         Sx = np.dot(cov, x)
@@ -107,7 +107,7 @@ def solve_rb_ccd(
         bounds = np.array(bounds * 1.0)
 
     if budgets is None:
-        budgets = np.array([1] * n) / n
+        budgets = np.array([1.0] * n) / n
     else:
         budgets = np.array(budgets)
     budgets = budgets / np.sum(budgets)
